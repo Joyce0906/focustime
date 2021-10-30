58c4528a4c539099db2ff0e7e30fb4c606584c95
mel02 <melinajoyceestrada86@gmail.com>
2021-10-30 16:58:26 +0800

    Initial Entry

---
+++ src/utils/colors.js
@@ -0,0 +1,10 @@
+export const colors ={
+  white: '#fff',
+  darkmoderateblue: '#256378',
+  darkblue: "00008b", 
+  red: "#ff0000",
+  yellow: "#ffff00",
+  black: "#000",
+  darkorange: "#FF8C00",
+  orange: "#ffa500",
+}
